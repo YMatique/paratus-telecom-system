cb1599153d883e30387d7d8b4eece049e37fe5f8
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-08-29 08:24:42 +0200

    creating initiaals components

---
+++ app/Livewire/PlanManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PlanManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.plan-manager');
+    }
+}
