d52a20b3e0f5989745d9ad56ce5cd63f7ed6286d
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-09-02 11:56:49 +0200

    adding example of usage

--- app/Livewire/ClientManager.php
+++ app/Livewire/ClientManager.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Concerns\WithToast;
 use Livewire\Component;
 
 class ClientManager extends Component
 {
+     use WithToast;
+
+    public function save()
+    {
+        // Validação e salvamento...
+        
+        $this->toastSuccess('Cliente salvo!', 'Dados atualizados com sucesso.');
+    }
+
     public function render()
     {
         return view('livewire.client-manager');
