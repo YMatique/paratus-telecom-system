8adc3129fedc92bd034d56b5dc2f755bac258ec3
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-09-02 15:55:55 +0200

    changing app.css removing opacity-500 for tailwindcss 4.

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    #[Layout('livewire.layouts.app')]
     public function render()
     {
         return view('livewire.dashboard');
